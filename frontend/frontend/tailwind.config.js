140ee4130d3bed951a013ecd681cc3669f35842b
otman545 <otman91540@outlook.fr>
2026-06-30 12:19:05 +0200

    Add files via upload

---
+++ frontend/frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#0055A4",
+          hover: "#004080",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
